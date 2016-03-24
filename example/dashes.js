90c1b114906b9faa86f3a7fe669e976efee225eb
CubiZm <seo-xcube@yandex.ru>
2016-03-24 22:34:47 +0300

    new result

--- example/dashes.js
+++ example/dashes.js
@@ -2,13 +2,22 @@
 
 var number = 2212062344; // 2-212-0-6-234-4
 // Преобразуем их в строку (чтобы вставлять "-" потом)
-var string = number.toString();
+// var string = number.toString();
 
 function dashes() {
+
 	var string = number.toString();
-	// через определенную "букву" добавляем "-"
-	var newString = string[0] + "-" + string[1] + string[2] + string[3] + "-" + string; //и т.д. В идеале будет циклом.
-	return newString;
+	//Пихнем в массив
+	var arrString = string.split("");
+	// через определенную "букву" добавляем "-" в цикле
+	for (i = 0; i < arrString.length; i++) {
+		if (i % 2) {
+			return arrString[i] + "-";
+		};
+		// return arrString += "-";
+	}
+
+	// return arrString;
 
 }
 
