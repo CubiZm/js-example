b43ba24e52e5f378b498f6aa3f3d8802aa885884
Lena <seo-xcube@yandex.ru>
2016-03-25 15:41:14 +0400

    ...

--- example/removeLetters.js
+++ example/removeLetters.js
@@ -6,16 +6,12 @@ function removeLetters(words) {
 
  var result = '';
 
- //var letters =  'аеёиоуэюя';
+//var letters =  'аеёиоуэюя';
 var letters = ["а", "е", "ё", "и", "о", "у", "э", "ю", "я"]
 
 
   for (var i = 0; i < arr.length; i++) {
-    result += arr[i];
-
-    if (arr[i] === letters) {
-      result = result + "-";
-    }
+    result = arr[i];
   }
   return result;
 }
