adf94eea4071f24f71745276d5fd16356f619fc3
CubiZm <seo-xcube@yandex.ru>
2016-03-25 22:58:43 +0300

    add function

---
+++ example/repeat.js
@@ -0,0 +1,27 @@
+function repeat (firstNum, secondNum) {
+	//var str = firstNum.toString() + secondNum.toString();
+ 	// var a = firstNum + secondNum
+	// var arr = str.split('');
+	var result = '';
+
+	for (var i = 0; i < secondNum.length; i++) {
+		result += arr[i];
+	}
+	return result;
+}
+
+repeat(1, 5);
+
+
+
+// for (var i = 0; i < 3; i++) {
+//   alert(i); // 0, 1, 2
+// }
+
+function repeat(a, b) {
+	var res = '';
+	for (; a < b; a++) {
+		console.log(a[a]);
+	}
+	return res;
+}
